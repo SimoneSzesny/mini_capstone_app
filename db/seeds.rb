3c60a92f9d1014d9f37d7804be2f136b1afef68a
Simone <simoneszesny@gmail.com>
2017-03-07 20:19:27 -0600

    5 chairs added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-chair = Chair.new(name:"Truss Lounge Chair", image:"http://www.yliving.com/images/gus-truss-lounge-chair/gallery_3.jpg", price: 895, description: "Inspired by mid-century design, the Truss Chair is a comfortable accent chair suitable for small spaces. Named for the wood truss style construction detail on the sides, the Truss Chair features a structured silhouette with an architectural influence.")
+# chair = Chair.new(name:"Truss Lounge Chair", image:"http://www.yliving.com/images/gus-truss-lounge-chair/gallery_3.jpg", price: 895, description: "Inspired by mid-century design, the Truss Chair is a comfortable accent chair suitable for small spaces. Named for the wood truss style construction detail on the sides, the Truss Chair features a structured silhouette with an architectural influence.")
+
+# chair.save
+
+chair = Chair.new(name:"Smith", image:"https://cdn.shopify.com/s/files/1/0400/4557/products/Barnaby_lane_Product-5_medium.jpg?v=1488931913", price: 849, description: "The Smith chair is our most popular - it's pretty obvious why! This sling leather chair is a stunning feature to any living room, bedroom or study. Available in natural, black or raw tan. Every home should have a Smith.")
+
+chair.save
+
+chair = Chair.new(name:"Bellview Fabric Retro Chair", image:"https://st.hzcdn.com/simgs/cd31fbb803bf2a40_4-9271/midcentury-armchairs-and-accent-chairs.jpg", price: 275, description: "Add some retro vibes to any space with the Bellview Accent Chair. This piece is designed with style in mind. The color, and texture gives this chair a unique overall look and the curved back and armrests provide great comfort. If you're looking for comfort and style, the Bellview is your chair. ")
+
+chair.save
+
+chair = Chair.new(name:"The Peggy", image:"https://thumbor-gc.tomandco.uk/unsafe/fit-in/628x360/center/middle/filters:fill(white)/www.sofa.com/static/products/large/FSPEG110STWFLM.jpg", price: 590, description: "The Peggy armchair – which reminds us more of a cocktail or club chair with its retro looks. Its curved arms, cylindrical wood legs, shallow seat and slanted back combine for a petite but elegant chair that can be customised in a huge range of prints, patterns and textures. The frame has a lifetime guarantee and delivery takes four to six weeks. ")
+
+chair.save
+
+chair = Chair.new(name:"George Armchair", image:"https://files.neptune.com//GetImage.ashx?imageGuid=fec76889-6c73-4cc7-9609-f6c27df23361&width=510&height=510", price: 910, description: "Like its namesake, the George is a classic English design that has modern appeal. By mixing a contemporary aesthetic with an original design we have subtly reinterpreted a traditional style to give it a modern London club feel. The low profile, large rolling arms and deep back make it comfortable and sophisticated. Perfect for a prince or princess.")
 
 chair.save
 
