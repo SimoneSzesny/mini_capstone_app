b87f5c236f97bafe60bd54d47fe052811b8cd9da
Simone <simoneszesny@gmail.com>
2017-03-09 21:28:29 -0600

    add index 3/9

--- app/controllers/chairs_controller.rb
+++ app/controllers/chairs_controller.rb
@@ -1,6 +1,11 @@
 class ChairsController < ApplicationController
-  def chair1
+  def index
     @chairs = Chair.all
     render "chairs.html.erb"
   end
+  def show
+    chairs_id = params[:id]
+    @chairs = Chair.find_by(id: chair_id)
+    render "show.html.erb"
+end
 end
