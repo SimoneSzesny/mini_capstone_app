8f087f8e8dc54e40bf00b717965352a93f30030a
Simone <simoneszesny@gmail.com>
2017-03-12 10:28:31 -0500

    add submit form

--- app/controllers/chairs_controller.rb
+++ app/controllers/chairs_controller.rb
@@ -7,5 +7,23 @@ class ChairsController < ApplicationController
     chair_id = params[:id]
     @chair = Chair.find_by(id: chair_id)
     render "show.html.erb"
+  end
+ 
+ def new
+  render "new.html.erb"
 end
+
+def create
+  chair = Chair.new(
+  name: params[:form_name],
+  image: params[:form_image],
+  price: params[:form_price], 
+  description: params[:form_desription]
+  )
+  chair.save
+  render "create.html.erb"
+end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/chairs" => "chairs#index"
+  get "/chairs/new" => "chairs#new"
+  post "/chairs" => "chairs#create"
   get "/chairs/:id" => "chairs#show"
 end
